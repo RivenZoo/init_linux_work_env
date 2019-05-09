fecbdaa8f4e1faeef7cfe1773121f4e7617bd093
zrw <zrw2010@gmail.com>
2019-05-09 16:52:42 +0800

    feat: add init script
    Signed-off-by: zrw <zrw2010@gmail.com>

---
+++ opensuse/fzf.sh
@@ -0,0 +1,7 @@
+# install fzf in ~/.fzf
+
+cd ~
+mkdir ~/.fzf
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
---
+++ opensuse/tools.sh
@@ -0,0 +1,11 @@
+# install common tools
+sudo zypper in git
+sudo zypper in docker
+sudo zypper in fd
+
+sudo zypper ar https://download.opensuse.org/repositories/utilities/openSUSE_Leap_15.0/utilities.repo
+sudo zypper install ripgrep
+
+sudo zypper in htop
+sudo zypper in ncdu
+
